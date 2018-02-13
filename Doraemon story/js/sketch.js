e43fffcef848b69dde7f665e232f27ee27f9398f
vickyfan2018 <35770738+vickyfan2018@users.noreply.github.com>
2018-02-13 14:00:23 -0500

    changes

--- Doraemon story/js/sketch.js
+++ Doraemon story/js/sketch.js
@@ -12,14 +12,6 @@ $(function() {
         });
     });
 
-var draggable = document.getElementById('draggable');
-  draggable.addEventListener('touchmove', function(event) {
-    var touch = event.targetTouches[0];
-    
-    // Place element where the finger is
-    draggable.style.left = touch.pageX-25 + 'px';
-    draggable.style.top = touch.pageY-25 + 'px';
-    event.preventDefault();
-  }, false);
+
 
     
