0bb5cbe7fb06cc44db7344783936101b9e598e2e
vickyfan2018 <35770738+vickyfan2018@users.noreply.github.com>
2018-02-20 10:01:04 -0500

    modify

--- Doraemon story/js/sketch-for-disabled.js
+++ Doraemon story/js/sketch-for-disabled.js
@@ -1,12 +1,26 @@
 $(document).ready(function(){
        
-       $("#draggable").click(function(){
-        $("#error").fadeIn();
+       $("#draggable").draggable(function(){
+        
+            
+        });
+$( init );
+       function init() {
+  $('#draggable').draggable( {
+stop: handleDragStop
+  } );
+}	
+    });
+
+function handleDragStop( event, ui ) {
+  var offsetXPos = parseInt( ui.offset.left );
+  var offsetYPos = parseInt( ui.offset.top );
+  $("#error").fadeIn();
         setTimeout(function () {
        window.location.href = "fail.html";
     }, 1000); 
-            
-        });
-    });
+}
+
+
 
     
