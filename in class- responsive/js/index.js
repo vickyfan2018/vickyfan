2003e60b29fdb7fe58b593f85cb68629fde16ee2
vickyfan2018 <35770738+vickyfan2018@users.noreply.github.com>
2018-02-21 11:34:36 -0500

    responsive

---
+++ in class- responsive/js/index.js
@@ -0,0 +1,13 @@
+function myFunction() {
+    var x = document.getElementById("top-nav");
+    if (x.className === "nav-list") {
+        x.className += " responsive";
+    } else {
+        x.className = "nav-list";
+    }
+}
+
+
+
+
+
